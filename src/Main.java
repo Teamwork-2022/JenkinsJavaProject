2b8fff9bac63c5193413187ac23c29faf5436fbb
Wellstudy1985 <Nnoorzai.001@gmail.com>
2023-03-26 15:13:29 -0700

    new code for passed the test

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         for (int i = 0; i <10 ; i++) {
-            System.out.println("Hello world!");
-            System.out.println("Cheeka Cho");
-            System.out.println("Lee Lala Lee ");
-            System.out.println("Ho Ho Ha Hee Ha Hee");
+            System.out.println("Passed The Test");
+
             Thread.sleep(4000);
         }
 
